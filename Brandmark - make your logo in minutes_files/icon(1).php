c33be9ef34f7e431145a27f400e791eb42c57596
AzizbekSaidkarimov2008 <86466353+AzizbekSaidkarimov2008@users.noreply.github.com>
2021-06-25 15:33:14 +0500

    Add files via upload
    css

---
+++ Brandmark - make your logo in minutes_files/icon(1).php
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   id="svg81706"
+   viewBox="0 0 200 200"
+   height="200px"
+   width="200px"
+   version="1.1">
+  <metadata
+     id="metadata81712">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <defs
+     id="defs81710" />
+  <g
+     id="monogram"
+     style="font-style:normal;font-weight:normal;font-size:120px;line-height:1;font-family:Baloo;font-variant-ligatures:none;text-align:center;dominant-baseline:middle;text-anchor:middle;fill:#e2af39"
+     aria-label="a">
+    <path
+       id="path81714"
+       style="font-style:normal;font-weight:normal;font-size:120px;line-height:1;font-family:Baloo;font-variant-ligatures:none;text-align:center;dominant-baseline:middle;text-anchor:middle;fill:#e2af39"
+       d="m 98.504688,116.14 q 2.040002,0 4.440002,-0.36 2.52,-0.48 3.72,-1.2 v -9.6 l -8.640003,0.72 q -3.36,0.24 -5.519999,1.44 -2.160001,1.2 -2.160001,3.6 0,2.4 1.800001,3.96 1.92,1.44 6.36,1.44 z m -0.96,-47.16 q 6.480002,0 11.760002,1.32 5.4,1.32 9.12,4.08 3.84,2.64 5.88,6.84 2.04,4.08 2.04,9.72 v 26.88 q 0,3.12 -1.8,5.16 -1.68,1.92 -4.08,3.36 -7.8,4.68 -21.960002,4.68 -6.36,0 -11.52,-1.2 -5.040001,-1.2 -8.76,-3.6 -3.600001,-2.4 -5.640001,-6.12 -1.92,-3.72 -1.92,-8.64 0,-8.28 4.92,-12.72 4.920001,-4.44 15.24,-5.52 L 106.54469,91.54 V 90.7 q 0,-3.48 -3.12,-4.92 -3,-1.56 -8.760003,-1.56 -4.56,0 -8.879999,0.96 -4.320001,0.96 -7.8,2.4 -1.56,-1.08 -2.64,-3.24 -1.080001,-2.28 -1.080001,-4.68 0,-3.12 1.440001,-4.92 1.56,-1.92 4.679999,-3.24 3.480001,-1.32 8.16,-1.92 4.8,-0.6 9.000001,-0.6 z" />
+  </g>
+</svg>
